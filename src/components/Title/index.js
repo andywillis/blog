373f1402405d3528522ba7238ba2ada5b6f6d578
Andy Willis <awillis@fastmail.fm>
2023-07-12 00:00:01 +0100

    Fixed CSS

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,8 @@
 import Heading from '../Heading';
 
-import style from './index.module.css';
-
 export default function Title(content, link) {
 	return `
-		<a href="#${link}" class="${style.link}">
+		<a href="#${link}">
 			${Heading({ link, level: 'h2', type: 'h2', text: content })}
 		</a>
 	`;
