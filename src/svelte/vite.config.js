241b5fccf6819d3303773c5b0e323874182e7b45
Andy Willis <awillis@fastmail.fm>
2023-07-25 12:59:26 +0100

    Set svelte

---
+++ src/svelte/vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import postcssNesting from 'postcss-nesting';
+import compress from 'vite-plugin-compression';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    splitVendorChunkPlugin(),
+    compress()
+  ],
+  publicDir: '../public',
+  css: {
+    // modules: {
+    //  generateScopedName: (name) => name
+    // },
+    postcss: {
+      plugins: [
+        postcssNesting
+      ]
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    target: 'esnext',
+    emptyOutDir: true,
+    outDir: '../../build',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+})
